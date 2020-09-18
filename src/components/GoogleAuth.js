55266914bc77dfb226e529220189477f4a3f615f
vrishty <37357334+vrishty@users.noreply.github.com>
2020-09-18 10:23:39 +1000

    Google API configuration for logging in/out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -15,7 +15,7 @@ class GoogleAuth extends React.Component {
             });
         });
     }
-    onAuthChange = (isSignedIn) => {
+    onAuthChange = isSignedIn => {
         if (isSignedIn) {
             this.props.signIn(this.auth.currentUser.get().getId());
         } else {
